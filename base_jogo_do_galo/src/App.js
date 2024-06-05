f5ce9db9cd2fcb929d01dca0461b115b75377368
MadalenaAntunes <162154816+MadalenaAntunes@users.noreply.github.com>
2024-06-05 10:18:45 +0100

    Add files via upload

---
+++ base_jogo_do_galo/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { JogoDoGalo } from "./components/JogoDoGalo";
+import { Toggle } from "./components/Toggle";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Renderizar o JogoDoGalo */}
+      <JogoDoGalo />
+      {/* <Toggle /> */}
+    </div>
+  );
+}
+
+export default App;
